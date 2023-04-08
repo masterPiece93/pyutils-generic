3b23a1bda258fff2261e83adf068eeec471ccff8
Ankit Yadav <42907602+masterPiece93@users.noreply.github.com>
2023-04-08 15:34:07 +0530

    name change, extra implementation and docs
    added immutable dict implementation , added function strings , renamed the file

--- immutables.py
+++ immutables.py
@@ -3,6 +3,9 @@ import copy
 
 
 class ReadOnlyDictWrapper(collections.abc.Mapping):
+    """Wraps a dict
+    Provides Read-Only Functionalities
+    """
     def __init__(self, data: dict):
         if not isinstance(data, dict):
             raise Exception(f"{dict} expected . Got {type(data)}")
@@ -19,3 +22,21 @@ class ReadOnlyDictWrapper(collections.abc.Mapping):
 
     def __str__(self):
         return str(self._data)
+    
+ class imdict(dict):
+    """Dict implementation.
+    Blocks Mutable Operations
+    """
+    def __hash__(self):
+        return id(self)
+
+    def _immutable(self, *args, **kws):
+        raise TypeError('object is immutable')
+
+    __setitem__ = _immutable
+    __delitem__ = _immutable
+    clear       = _immutable
+    update      = _immutable
+    setdefault  = _immutable
+    pop         = _immutable
+    popitem     = _immutable
