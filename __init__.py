62e9fb5979a681cefb8efeba0c08d6787370303c
Ankit Yadav <42907602+masterPiece93@users.noreply.github.com>
2023-04-09 11:24:28 +0530

    Update __init__.py
    removing the unnecessary changes done in last to last commit

--- __init__.py
+++ __init__.py
@@ -1,4 +1,3 @@
-from collections import namedtuple
 from functools import wraps
 
 
@@ -13,41 +12,3 @@ def elf(func):
 
     return wrapper
 
-class Colors:
-    _HEADER = "\033[95m"
-    _BLUE = "\033[94m"
-    _CYAN = "\033[96m"
-    _GREEN = "\033[92m"
-    _WARNING = "\033[93m"
-    _FAIL = "\033[91m"
-    _ENDC = "\033[0m"
-    _BOLD = "\033[1m"
-    _UNDERLINE = "\033[4m"
-
-    @classmethod
-    def cprint(cls, *args, **kwargs):
-        """Provides functions for various color printing."""
-        colorfunc = namedtuple(
-            "colorfunc",
-            """
-                header,
-                blue,
-                cyan,
-                green,
-                warning,
-                fail,
-                bold,
-                underline
-            """,
-        )
-
-        return colorfunc(
-            lambda: print(cls._HEADER, *args, cls._ENDC, **kwargs),
-            lambda: print(cls._BLUE, *args, cls._ENDC, **kwargs),
-            lambda: print(cls._CYAN, *args, cls._ENDC, **kwargs),
-            lambda: print(cls._GREEN, *args, cls._ENDC, **kwargs),
-            lambda: print(cls._WARNING, *args, cls._ENDC, **kwargs),
-            lambda: print(cls._FAIL, *args, cls._ENDC, **kwargs),
-            lambda: print(cls._BOLD, *args, cls._ENDC, **kwargs),
-            lambda: print(cls._UNDERLINE, *args, cls._ENDC, **kwargs),
-        )
