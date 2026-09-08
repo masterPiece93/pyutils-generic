0d60f19fdd6a3045c0a8bf72f1a37cd6e6401be2
masterPiece93 <ankit8290@gmail.com>
2026-09-08 14:09:22 +0530

    (colored) updated examples in colored module

--- examples/printing_coloured.py
+++ examples/printing_coloured.py
@@ -3,36 +3,36 @@ Example usage of the cprint function with colored output.
 """
 
 
-from pyutils.printing import cprint, Colors
+from pyutils.colored import cprint, Palette
 from typing import NamedTuple
 
 # classic usage of cprint
-cprint("Hello, World!").header()
-cprint("This is a blue message.").blue()
-cprint("This is a green message.").green()
-cprint("This is a warning message.").warning()
-cprint("This is an error message.").fail()
-cprint("This is a bold message.").bold()
-cprint("This is an underline message.").underline()
+cprint("Hello, World!").style(B=True)
+cprint("This is a blue message.").style(Palette.BLUE)
+cprint("This is a green message.").style(Palette.GREEN)
+cprint("This is a warning message.").style(Palette.YELLOW)
+cprint("This is an error message.").style(Palette.RED)
+cprint("This is a bold message.").style(Palette.BOLD)
+cprint("This is an underline message.").style(Palette.UNDERLINE)
 
 # registering new colors
-Colors.register("ORANGE_BOLD", "\033[38;5;208m\033[1m")
-Colors.register("PURPLE_UNDERLINE", "\033[38;5;129m\033[4m")
+Palette.register("ORANGE_BOLD", "\033[38;5;208m\033[1m")
+Palette.register("PURPLE_UNDERLINE", "\033[38;5;129m\033[4m")
 
 # a usage pattern to use the colorized string in a more pythonic way
 class C(NamedTuple):
     """
     A class to represent a colorized string.
 
-    >>> C("This text will be green", Colors.GREEN)
+    >>> C("This text will be green", Palette.GREEN)
     This text will be green
     """
-    red: str = lambda text: Colors.RED.colorize(text)
-    green: str = lambda text: Colors.GREEN.colorize(text)
-    blue: str = lambda text: Colors.BLUE.colorize(text)
-    B: str = lambda text: Colors.BOLD.colorize(text)
-    U: str = lambda text: Colors.UNDERLINE.colorize(text)
-    I: str = lambda text: Colors.ITALIC.colorize(text)
+    red: str = lambda text: Palette.RED.colorize(text)
+    green: str = lambda text: Palette.GREEN.colorize(text)
+    blue: str = lambda text: Palette.BLUE.colorize(text)
+    B: str = lambda text: Palette.BOLD.colorize(text)
+    U: str = lambda text: Palette.UNDERLINE.colorize(text)
+    I: str = lambda text: Palette.ITALIC.colorize(text)
 
 c = C()
 c.green("This text will be green")
@@ -42,12 +42,12 @@ c.U("This text will be underlined")
 c.I("This text will be italic")
 
 # directly using the colorized string in a more pythonic way
-print(Colors.GREEN.colorize("This text will be green"))
-print(Colors.RED.colorize("This text will be red"))
-print(Colors.BOLD.colorize("This text will be bold"))
-print(Colors.UNDERLINE.colorize("This text will be underlined"))
-print(Colors.ITALIC.colorize("This text will be italic"))
+print(Palette.GREEN.colorize("This text will be green"))
+print(Palette.RED.colorize("This text will be red"))
+print(Palette.BOLD.colorize("This text will be bold"))
+print(Palette.UNDERLINE.colorize("This text will be underlined"))
+print(Palette.ITALIC.colorize("This text will be italic"))
 
 # A semi coloured printing example
-print("This is a message with multiple colors: " + Colors.GREEN.colorize("green") + ", " + Colors.RED.colorize("red") + ", and " + Colors.BLUE.colorize("blue") + ".")
-print(f"This is a message with multiple colors: {Colors.GREEN.colorize("green")} , {Colors.RED.colorize("red")} and {Colors.BLUE.colorize("blue")} with fstrings.")
+print("This is a message with multiple colors: " + Palette.GREEN.colorize("green") + ", " + Palette.RED.colorize("red") + ", and " + Palette.BLUE.colorize("blue") + ".")
+print(f"This is a message with multiple colors: {Palette.GREEN.colorize('green')} , {Palette.RED.colorize('red')} and {Palette.BLUE.colorize('blue')} with fstrings.")
